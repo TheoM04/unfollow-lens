e5dba7a47fc78ec1a06c742ffa0027d399b70071
TheoM04 <theodoremaleas@gmail.com>
2026-09-14 17:44:51 +0300

    First web page, accepts only json files and outputs the content. Start with node (serve)

---
+++ script.js
@@ -0,0 +1,25 @@
+const fileInput = document.getElementById("followers-input");
+const outputLabel = document.getElementById("output-label");
+
+fileInput.addEventListener('change', function (event) {
+    // event.target.files contains the list of files selected by the user
+    const file = event.target.files[0];
+
+    // Make sure a file was actually selected in case they clicked cancel
+    if (!file) {
+        return;
+    }
+
+    // read the file
+    const reader = new FileReader();
+
+    // This function runs automatically once the file is finished loading
+    reader.onload = function (e) {
+        const rawText = e.target.result;
+        const parsedData = JSON.parse(rawText);
+        console.log("Successfully parsed JSON data:", parsedData.Greeting);
+    };
+
+    // Start reading the file as plain text
+    reader.readAsText(file);
+});
